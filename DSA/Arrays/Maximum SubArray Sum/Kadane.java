e4ad67211ef847e1c1f738c3d32c6cc1d256c5a9
Deeptansh Nagar <diptanshnagar@gmail.com>
2026-01-04 03:43:55 +0530

    dsa

--- DSA/Arrays/Maximum SubArray Sum/Kadane.java
+++ DSA/Arrays/Maximum SubArray Sum/Kadane.java
@@ -1,5 +1,11 @@
 public class Kadane {
 	public static void main(String[] args) {
+		int[] arr = {-2, -3, 4, -1, -2, 1, 5, -3};
+		int ans = maxSubArr(arr);
+		System.out.println(ans);
+	}
+
+	static int maxSubArr(int[] arr) {
 		
 	}
 }
--- DSA/Arrays/Maximum SubArray Sum/better.java
+++ DSA/Arrays/Maximum SubArray Sum/better.java
@@ -1,5 +1,19 @@
 public class better {
 	public static void main(String[] args) {
-		
+		int[] arr = {-2, -3, 4, -1, -2, 1, 5, -3};
+		int ans = maxSubArr(arr);
+		System.out.println(ans);
+	}
+	static int maxSubArr(int[] arr) {
+		int maxSum = Integer.MIN_VALUE;
+        for (int i = 0; i < arr.length; i++) {
+			int sum = 0;
+            for (int j = i; j < arr.length; j++) {
+                sum = sum + arr[j];
+				maxSum = Math.max(maxSum, sum);
+            }
+        }
+        return maxSum;
 	}
 }
+
